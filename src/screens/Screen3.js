3f21ba57a2403598b7532b56e318e0cfaec7a401
Jupiter836 <jupiter836@icloud.com>
2021-04-04 21:11:36 +0300

    upd5

--- src/screens/Screen3.js
+++ src/screens/Screen3.js
@@ -1,31 +1,71 @@
 import React from 'react'
-import { ScrollView, StyleSheet } from 'react-native'
-import { UserCard } from '../components/UserCard'
-import { DATA } from '../DATA'
+import { ScrollView, StyleSheet, Image, Text, View } from 'react-native'
+import Waterfall from '../assets/images/Waterfall.png'
 
-export const Screen1 = () => {
+const GalleryBlock = () => {
     return (
-      <ScrollView style={styles.scrollStyle} contentContainerStyle={styles.scrollContainer}>
-        {DATA.map((item) => (
-          <UserCard
-            userName={item.userName}
-            userPhone={item.userPhone}
-            userPic={item.userPic}
-            userAddress={item.addressDescription}
-            userMail={item.emailDescription}
-          />
-        ))}
-      </ScrollView>
+      <>
+        <View style={styles.root}>
+          <View style={styles.cardStyle}>
+            <Image style={styles.iconStyle} source={Waterfall} />
+            <Text style={styles.galleryStyle}>Official photo for album</Text>
+          </View>
+        </View>
+      </>
+  )
+}
+
+export const Screen3 = () => {
+    return (
+      <>
+        <ScrollView style={styles.scrollStyle} contentContainerStyle={styles.scrollContainer}>
+          <GalleryBlock />
+          <GalleryBlock />
+          <GalleryBlock />
+          <GalleryBlock />
+          <GalleryBlock />
+          <GalleryBlock />
+          <GalleryBlock />
+          <GalleryBlock />
+        </ScrollView>
+      </>
     )
 }
 
 const styles = StyleSheet.create({
     scrollStyle: {
-      flex: 1
+        
+        
+        
     },
     scrollContainer: {
-      paddingTop: 20,
-      alignItems: 'center',
-      justifyContent: 'center'
-    }
-})
+    paddingTop:37,
+    alignItems: 'center',
+    justifyContent: 'center'
+    
+    },
+    cardStyle: {
+        width: 363,
+        height: 172,
+        backgroundColor: '#0A0A0A',
+        borderWidth: 1,
+        borderRadius: 20,
+        borderColor: '#444444',
+        marginBottom: 20
+    },
+   iconStyle: {
+       width: 339,
+       height: 118,
+       borderRadius: 5,
+       marginTop: 13,
+       marginLeft: 12
+   },
+   galleryStyle:{
+    fontSize: 12,
+    fontWeight: '500',
+    color: '#FFFFFF',
+    marginTop: 14,
+    marginLeft: 12,
+    borderColor: '#444444'
+   }
+  })
